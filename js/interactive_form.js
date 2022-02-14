96749129c204c8a1a98abca9e891ac9c6b874a7d
sik9252 <sik9252@naver.com>
2022-02-14 22:14:18 +0900

    interactive한 form 만들기 실습

--- js/interactive_form.js
+++ js/interactive_form.js
@@ -1,7 +1,28 @@
+let shirtSize = [90, 95, 100, 105, 110];
+let pantsSize = [24, 26, 28, 30, 32, 34, 36];
+
 $("#option1").on("change", function () {
+  $("#option2").html("");
+  $(".select-size").show();
   if ($("#option1").val() == "셔츠") {
-    $(".select-size").show();
-  } else if ($("#option1").val() == "모자") {
+    // for (i = 0; i < shirtSize.length; i++) {
+    //   let selectTemplate = `<option>${shirtSize[i]}</option>`;
+    //   $("#option2").append(selectTemplate);
+    // }
+    shirtSize.forEach(function (i) {
+      let selectTemplate = `<option>${i}</option>`;
+      $("#option2").append(selectTemplate);
+    });
+  } else if ($("#option1").val() == "바지") {
+    // for (i = 0; i < pantsSize.length; i++) {
+    //   let selectTemplate = `<option>${pantsSize[i]}</option>`;
+    //   $("#option2").append(selectTemplate);
+    // }
+    pantsSize.forEach(function (i) {
+      let selectTemplate = `<option>${i}</option>`;
+      $("#option2").append(selectTemplate);
+    });
+  } else {
     $(".select-size").hide();
   }
 });
