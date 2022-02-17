dd6b621d88da97ed1e42d392a58fb626110b3591
sik9252 <sik9252@naver.com>
2022-02-17 21:29:32 +0900

    상품 정렬 & 필터링 실습

--- js/shoppingmall.js
+++ js/shoppingmall.js
@@ -11,8 +11,12 @@ products.forEach((product) => {
   $(".product-price").eq(id).html(`가격: ${products[id].price}`);
 });
 
-// 상품 가격 오름차순 정렬
-$("#sort-cheap").click(function () {
+// 상품 가격 낮은순 정렬
+$("#btn1").click(function () {
+  if ($("#card-group1").hide()) {
+    $("#card-group1").show();
+    $("#card-group2").hide();
+  }
   products.sort(function (a, b) {
     return a.price - b.price;
   });
@@ -24,8 +28,12 @@ $("#sort-cheap").click(function () {
   });
 });
 
-// 상품 가격 내림차순 정렬
-$("#sort-expensive").click(function () {
+// 상품 가격 높은순 정렬
+$("#btn2").click(function () {
+  if ($("#card-group1").hide()) {
+    $("#card-group1").show();
+    $("#card-group2").hide();
+  }
   products.sort(function (a, b) {
     return b.price - a.price;
   });
@@ -35,4 +43,50 @@ $("#sort-expensive").click(function () {
     $(".product-name").eq(id).html(products[id].title);
     $(".product-price").eq(id).html(`가격: ${products[id].price}`);
   });
+});
+
+// 상품 가나다순 정렬
+$("#btn3").click(function () {
+  if ($("#card-group1").hide()) {
+    $("#card-group1").show();
+    $("#card-group2").hide();
+  }
+  products.sort((a, b) => {
+    if (a.title < b.title == true) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  products.forEach((product) => {
+    const id = product.id;
+    $(".product-name").eq(id).html(products[id].title);
+    $(".product-price").eq(id).html(`가격: ${products[id].price}`);
+  });
+});
+
+// 6만원 이하만 보기
+$("#btn4").click(function () {
+  $("#card-group1").hide();
+  $("#card-group2").show();
+
+  const newProducts = products.filter((a) => {
+    return a.price <= 60000;
+  });
+
+  if ($("#card-group2").empty()) {
+    newProducts.forEach((product) => {
+      const id = product.id;
+      const template = `<div class="card">
+    <img src="https://via.placeholder.com/600" />
+    <div class="card-body">
+      <h5 class="product-name">${newProducts[id - 1].title}</h5>
+      <p class="product-price">${newProducts[id - 1].price}</p>
+      <button class="btn btn-danger">주문하기</button>
+    </div>
+  </div>`;
+      $("#card-group2").append(template);
+    });
+  }
 });
